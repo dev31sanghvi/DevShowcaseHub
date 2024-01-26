f13eb9434c957a662c2c4f4aa6ba6090164da7fb
Dev Sanghvi <105737978+dev31sanghvi@users.noreply.github.com>
2024-01-26 18:56:34 +0530

    Update Headesr.jsx

--- src/components/Headesr.jsx
+++ src/components/Headesr.jsx
@@ -4,11 +4,11 @@ import React from 'react';
 const Header = () => {
   return (
     <header className="bg-black p-4 flex items-center justify-between">
-      {/* Logo (Centered) */}
+      {/* Logo */}
       <div className="text-white text-lg font-bold mx-auto">DevShowcaseHub</div>
 
 
-      {/* Event Calendar Button (Right) */}
+      {/* Event Calendar Button */}
       <a
         href="#event-calendar"
         className="text-white hover:text-gray-200 transition duration-300"
